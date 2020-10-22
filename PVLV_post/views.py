d7657cd8b85ad6e3a397e56637aebd9c291f66ba
Ksaw303x <smn.not@outlook.com>
2020-10-22 20:29:54 +0200

    moved templates to the website template folder

--- PVLV_post/views.py
+++ PVLV_post/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    UpdateView,
+)
+
+from .models import PostGeneratorConfig
 
 
 def post(request):
-
-    context = {
-        'spot': {
-        }
-
-    }
+    context = {}
     return render(request, 'console/post.html', context)
+
+
+class PostView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """Update the post settings"""
+    model = PostGeneratorConfig
+    template_name = 'console/post.html'
+    fields = ['name', 'post', 'spot']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        config = self.get_object()
+        if self.request.user == config.author:
+            return True
+        return False
